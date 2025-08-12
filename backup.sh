ba2a658ee5244b8de40f734695e4f978b5f3d319
Deyan Stoyanov <deyanstoyanov850618@gmail.com>
2025-08-12 17:25:23 +0300

    Fix the backup script. Add the dove post.

--- backup.sh
+++ backup.sh
@@ -1,28 +1,31 @@
-rm -rf ../backup/ || echo "Nothing to delete"
-mkdir -p ../backup/blog
-cp ~/Desktop/passwords.kdbx ../backup/
-cp -r . ../backup/blog/
-rm -rf ../backup/blog/_site
-rm -rf ../backup/blog/.jekyll-cache/
-rm -rf ../backup/blog/.git/
-find ../backup/blog/ -type f -name '*.*~' -delete
-find ../backup/blog/ -type f -name '#*' -delete
+rm -rf backup/ || echo "No backup/ directory to delete."
+rm backup-*.zip || echo "No backup archives to delete."
+mkdir -p backup/blog
+cp ~/Desktop/passwords.kdbx backup/
+rsync -ax --exclude backup/ . backup/blog
+rm -rf backup/blog/_site
+rm -rf backup/blog/.jekyll-cache/
+rm -rf backup/blog/.git/
+find backup/blog/ -type f -name '*.*~' -delete
+find backup/blog/ -type f -name '#*' -delete
 
 ack \
 --ignore-dir _site \
 --ignore-dir .jekyll-cache \
+--ignore-dir .git \
+--ignore-dir backup \
 --no-filename \
 . \
 -o \
---match 'https://[a-zA-Z0-9_\-./=]+no' > ../backup/urls.txt
+--match 'https://[a-zA-Z0-9_\-./=]+no' > backup/blog/urls.txt
 
-mkdir -p ../backup/images
-wget -q --random-wait --content-disposition -P ../backup/images -i ../backup/urls.txt
+mkdir -p backup/blog/images
+wget -q it--random-wa --content-disposition -P backup/blog/images -i backup/blog/urls.txt
 
-zip -r ../backup$(date +"-%d-%m-%Y").zip ../backup/
+zip -r backup$(date +"-%d-%m-%Y").zip backup/
 
 #clean up
-rm -rf ../backup/
+rm -rf backup/
 
 #list backups
-ls -lah ../backup-*
+ls -lah backup-*
